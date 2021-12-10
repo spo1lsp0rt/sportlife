75695f07a811538756832288d474c4b7664b8d32
spo1lsp0rt <leha-dp@mail.ru>
2021-12-10 17:28:53 +0400

    Изменена вкладка "О сервисе"

--- laravel/resources/views/about.blade.php
+++ laravel/resources/views/about.blade.php
@@ -2,8 +2,14 @@
 
 @section('title')О нас@endsection
 
+@section('stylesheet')
+    <link rel="stylesheet" type="text/css" href="{{url('css/about.css')}}">
+@endsection
+
 @section('main_content')
 
-
+    <div class="ratio ratio-16x9">
+        <iframe src="/img/jum-jum.mp4" title="YouTube video" allowfullscreen></iframe>
+    </div>
 
 @endsection
