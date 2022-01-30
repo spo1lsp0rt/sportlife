b90e3513d9057b10b0d899ddc6539644d8e2933b
spo1lsp0rt <leha-dp@mail.ru>
2022-01-30 19:50:15 +0400

    Тут я что то сделал, но я не знаю что

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SportLife - @yield('title')</title>
-    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     @yield('stylesheet')
     <link rel="stylesheet" href="{{url('css/style.css')}}">
     <link rel="icon" href="/icons/favicon.svg">
@@ -71,7 +71,9 @@
         Позвоните нам 8-800-555-01-21
     </div>
 </footer>
-<script src="{{ mix('js/app.js') }}"></script>
-<script src="{{ mix('js/hamburger.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/hamburger.js') }}"></script>
 </body>
 </html>
