661b12e70ede5d54976e609fd06dd7e72a6d97fb
Shiriru <sakuradezkol@gmail.com>
2022-03-14 21:06:27 +0400

    Забыл докинуть файл третьего теста

---
+++ laravel/resources/views/task3.blade.php
@@ -0,0 +1,88 @@
+
+@php
+    $i = 1;
+    $course = '';
+@endphp
+
+<div class="result_title"><h2>Результаты тестирования</h2></div>
+
+<div class='container result_table'>
+    <div class='row'>
+        <div class='col col-md-4'>
+            <div class='title_field'>Проба</div>
+        </div>
+        <div class='col col-md-2'>
+            <div class='title_field_last'>Результат</div>
+        </div>
+        <div class='col col-md-3'>
+            <div class='title_field_last'>Оценка</div>
+        </div>
+        <div class='col col-md-3'>
+            <div class='title_field_last'>Диапазон</div>
+        </div>
+
+    </div>
+    @foreach($result->Exercises as $key => $exercise)
+        <div class='row'>
+            <div class='col col-md-4'>
+                <div class='data_field'>{{rtrim($exercise->Name, 'женмуж')}}</div>
+            </div>
+            <div class='col col-md-2'>
+                <div class='data_field data_field_last'>{{$exercise->Value}}</div>
+            </div>
+            <div class='col col-md-3'>
+                <div class='data_field data_field_last'>
+                    @php
+                        if($exercise->Norma == 1) { echo "1";}
+                        if($exercise->Norma == 2) { echo "2"; }
+                        if($exercise->Norma == 3) { echo "3"; }
+                        if($exercise->Norma == 4) { echo "4"; }
+                        if($exercise->Norma == 5) { echo "5"; }
+                    @endphp
+                </div>
+            </div>
+            <div class='col col-md-3'>
+                <div class='data_field data_field_last'>
+                    @php
+
+                        if(stripos($exercise->Name, 'муж'))
+                        {
+                            $norma = DB::select("select Value from normatives3 where gender = 'муж' AND id_exercise = ".$i+20);
+                        }
+                        else
+                        {
+                             $norma = DB::select("select Value from normatives3 where gender = 'муж' AND id_exercise = ".$i+20);
+                        }
+
+                        if($i != 4)
+                        {
+                            if($exercise->Value > $norma[0]->Value)
+                                echo $exercise->Value . " > " . $norma[0]->Value;
+                            else if ($exercise->Value < $norma[0]->Value && $exercise->Value > $norma[1]->Value)
+                                echo $norma[0]->Value . " --- " . $exercise->Value . " --- " . $norma[1]->Value;
+                            else if($exercise->Value < $norma[1]->Value && $exercise->Value > $norma[2]->Value)
+                                echo $norma[1]->Value . " --- " . $exercise->Value . " --- " . $norma[2]->Value;
+                            else if($exercise->Value < $norma[2]->Value && $exercise->Value > $norma[3]->Value)
+                                echo $norma[2]->Value . " --- " . $exercise->Value . " --- " . $norma[3]->Value;
+                            else if($exercise->Value < $norma[3]->Value)
+                                echo $exercise->Value . " < " . $norma[3]->Value;
+                        }else
+                        {
+
+                            if($exercise->Value > $norma[0]->Value)
+                                echo $exercise->Value . " > " . $norma[0]->Value;
+                            else if ($exercise->Value < $norma[0]->Value && $exercise->Value > $norma[1]->Value)
+                                echo $norma[0]->Value . " --- " . $exercise->Value . " --- " . $norma[1]->Value;
+                            else if($exercise->Value < $norma[1]->Value && $exercise->Value > $norma[2]->Value)
+                                echo $norma[1]->Value . " --- " . $exercise->Value . " --- " . $norma[2]->Value;
+                            else if($exercise->Value < $norma[2]->Value)
+                                echo $exercise->Value . " < " . $norma[3]->Value;
+                        }
+
+                        $i++;
+                    @endphp
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
