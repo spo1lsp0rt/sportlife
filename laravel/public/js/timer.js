3bf4801055a6170f9de31f01b362a3fb565dcc4d
Aranart <ilya.2001i@mail.ru>
2022-04-01 15:41:39 +0400

    Добавил js файл для таймера Вставил таймер в test.blade.php

---
+++ laravel/public/js/timer.js
@@ -0,0 +1,128 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*******************************!*\
+  !*** ./resources/js/timer.js ***!
+  \*******************************/
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+var FULL_DASH_ARRAY = 283;
+var WARNING_THRESHOLD = 10;
+var ALERT_THRESHOLD = 5;
+var COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+var TIME_LIMIT = 20;
+var timePassed;
+var timeLeft = TIME_LIMIT;
+var timerInterval = null;
+var remainingPathColor = COLOR_CODES.info.color;
+var timers = document.getElementsByClassName("timer");
+
+var _iterator = _createForOfIteratorHelper(timers),
+    _step;
+
+try {
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    var timer = _step.value;
+    timer.innerHTML = "\n<div class=\"base-timer\">\n  <svg class=\"base-timer__svg\" viewBox=\"0 0 100 100\" xmlns=\"http://www.w3.org/2000/svg\">\n    <g class=\"base-timer__circle\">\n      <circle class=\"base-timer__path-elapsed\" cx=\"50\" cy=\"50\" r=\"45\"></circle>\n      <path\n        id=\"base-timer-path-remaining".concat(timer.id.slice(5), "\"\n        stroke-dasharray=\"283\"\n        class=\"base-timer__path-remaining ").concat(remainingPathColor, "\"\n        d=\"\n          M 50, 50\n          m -45, 0\n          a 45,45 0 1,0 90,0\n          a 45,45 0 1,0 -90,0\n        \"\n      ></path>\n    </g>\n  </svg>\n  <span id=\"base-timer-label").concat(timer.id.slice(5), "\" class=\"base-timer__label\">").concat(formatTime(timeLeft), "</span>\n</div>\n");
+  }
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
+}
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+}
+
+var _iterator2 = _createForOfIteratorHelper(timers),
+    _step2;
+
+try {
+  for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+    var _timer = _step2.value;
+    console.log(_timer.id.slice(5));
+
+    var btnID = "timer_btn" + _timer.id.slice(5);
+
+    var btn = document.getElementById(btnID);
+    btn.addEventListener("click", setTimeout(startTimer, 1000), false);
+  }
+} catch (err) {
+  _iterator2.e(err);
+} finally {
+  _iterator2.f();
+}
+
+function startTimer() {
+  /*timePassed = 0;
+  timeLeft = TIME_LIMIT;
+  timerInterval = null;
+  remainingPathColor = COLOR_CODES.info.color;*/
+  timerInterval = setInterval(function () {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    console.log(document.activeElement.id);
+    var fullID = "base-timer-label" + document.activeElement.id.slice(9);
+    console.log(fullID);
+    document.getElementById(fullID).innerHTML = formatTime(timeLeft);
+    setCircleDasharray(document.activeElement.id.slice(9));
+    setRemainingPathColor(timeLeft, document.activeElement.id.slice(9));
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  var minutes = Math.floor(time / 60);
+  var seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = "0".concat(seconds);
+  }
+
+  return "".concat(minutes, ":").concat(seconds);
+}
+
+function setRemainingPathColor(timeLeft, id) {
+  var alert = COLOR_CODES.alert,
+      warning = COLOR_CODES.warning,
+      info = COLOR_CODES.info;
+
+  if (timeLeft <= alert.threshold) {
+    document.getElementById("base-timer-path-remaining" + id).classList.remove(warning.color);
+    document.getElementById("base-timer-path-remaining" + id).classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document.getElementById("base-timer-path-remaining" + id).classList.remove(info.color);
+    document.getElementById("base-timer-path-remaining" + id).classList.add(warning.color);
+  }
+}
+
+function calculateTimeFraction() {
+  var rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - 1 / TIME_LIMIT * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray(id) {
+  var circleDasharray = "".concat((calculateTimeFraction() * FULL_DASH_ARRAY).toFixed(0), " 283");
+  document.getElementById("base-timer-path-remaining" + id).setAttribute("stroke-dasharray", circleDasharray);
+}
+/******/ })()
+;
