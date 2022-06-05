234f4bb4a93216e4d1c555c771b2962269f646dd
Shiriru <sakuradezkol@gmail.com>
2022-06-05 11:50:38 +0400

    Убрал сохранение статистики у преподавателя и администратора после прохождения тестирования и блокировку теста

--- laravel/resources/views/test_result.blade.php
+++ laravel/resources/views/test_result.blade.php
@@ -44,20 +44,20 @@
     if(array_key_exists('ID_User', $_COOKIE))
     {
         $id = DB::table('user')->where('ID_User', $_COOKIE['ID_User'])->value('ID_Role');
+        $id_user_test = DB::table('statistic')->where('ID_Result', $result->ID_Result)->value('ID_User');
+        $id_role = DB::table('user')->where('ID_User', $id_user_test)->value('ID_Role');
     }
     @endphp
 
-    @if(!array_key_exists('ID_User', $_COOKIE))
+    @if(!array_key_exists('ID_User', $_COOKIE) || $id_role == 2 || $id_role == 3)
         @php
-            $id_user = DB::table('statistic')->where('ID_Result', $id)->value('ID_User');
-                $id_test = DB::table('statistic')->where('ID_Result', $id)->value('ID_Test');
-                DB::table('test_user')->where('ID_User', $id_user)->where('ID_Test', $id_test)->delete();
-                DB::table('statistic')->where('ID_Result', $id)->delete();
-                DB::table('result_exercises')->where('ID_Result', $id)->delete();
-                DB::table('results')->where('ID_Result', $id)->delete();
+            DB::table('test_user')->where('ID_User', $id_user_test)->where('ID_Test', $result->ID_Test)->delete();
+            DB::table('statistic')->where('ID_Result', $result->ID_Result)->delete();
+            DB::table('result_exercises')->where('ID_Result', $result->ID_Result)->delete();
+            DB::table('results')->where('ID_Result', $result->ID_Result)->delete();
         @endphp
     @endif
-    @if($id == 2)
+    @if($id == 2 && $id_role != 2)
     <form action="/clear_statistic/{{$result->ID_Result}}" method="post">
         @csrf
         <button formaction="/clear_statistic/{{$result->ID_Result}}" class="access_btn">Открыть доступ</button>
