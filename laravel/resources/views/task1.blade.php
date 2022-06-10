3a4a0565e4a9bca24773992e429d8ae686924f85
Илья Измайлов <ilya.2001i@mail.ru>
2022-06-10 18:11:55 +0300

    Переделал все таблицы результатов тестов и адаптировал их

--- laravel/resources/views/task1.blade.php
+++ laravel/resources/views/task1.blade.php
@@ -6,66 +6,63 @@
 
 <div class="result_title"><h2>Результаты тестирования</h2></div>
 
-    <div class='container statistic_table'>
-        <div class='row'>
-            <div class='col col-md-5'>
-                <div class='title_field_head'>Тест</div>
-            </div>
-            <div class='col col-md-3'>
-                <div class='title_field'>Результат</div>
-            </div>
-            <div class='col col-md-2'>
-                <div class='title_field'>НВП</div>
-            </div>
-            <div class='col col-md-2'>
-                <div class='title_field'>Оценка</div>
-            </div>
+<div class="container">
+    <div class="row">
+        <div class="table-responsive">
+            <table class="table table-bordered border-dark align-middle text-center">
+                <thead class="align-middle">
+                <tr>
+                    <th scope="col">Тест</th>
+                    <th scope="col">Результат</th>
+                    <th scope="col">НВП</th>
+                    <th scope="col">Оценка</th>
+                </tr>
+                </thead>
+                <tbody id="myTable" style="line-height: 2; white-space: normal;">
+                @foreach($result->Exercises as $key => $exercise)
+                    <tr>
+                        <th scope="row">{{$exercise->Name}}</th>
+                        <td>{{$exercise->Value}}</td>
+                        <td>{{$exercise->Norma}}</td>
+                        <td>
+                            @php
+                                if ($exercise->Name == "Ловля падающей линейки."){
+                                    $numerator = $exercise->Value - $exercise->Norma;
+                                    if ($numerator === 0.0) {
+                                        echo(round( $numerator / $exercise->Norma, 2));
+                                    }
+                                    else {
+                                        echo((-1) * round( $numerator / $exercise->Norma, 2));
+                                    }
+                                }
+                                else{
+                                    echo(round(( $exercise->Value - $exercise->Norma) / $exercise->Norma, 2));
+                                }
+                            @endphp
+                        </td>
+                    </tr>
+                    @php
+                        if ($exercise->Name == "Ловля падающей линейки.")
+                        {
+                            //dd($exercise);
+                            $OYFK += (-1) * round(( $exercise->Value - $exercise->Norma) / $exercise->Norma, 2);
+                        }
+                        else
+                            $OYFK += round(( $exercise->Value - $exercise->Norma) / $exercise->Norma, 2);
+                    @endphp
+                @endforeach
+
+                @php $OYFK = $OYFK / 7; @endphp
+                </tbody>
+            </table>
         </div>
-
-        @foreach($result->Exercises as $key => $exercise)
-            <div class='row'>
-                <div class='col col-md-5'>
-                    <div class='data_field_head textX_left'>{{$exercise->Name}}</div>
-                </div>
-                <div class='col col-md-3'>
-                    <div class='data_field'>{{$exercise->Value}}</div>
-                </div>
-                <div class='col col-md-2'>
-                    <div class='data_field'>{{$exercise->Norma}}</div>
-                </div>
-                <div class='col col-md-2'>
-                    <div class='data_field'>
-                        @php
-                            if ($exercise->Name == "Ловля падающей линейки."){
-                                $numerator = $exercise->Value - $exercise->Norma;
-                                if ($numerator === 0.0) {
-                                    echo(round( $numerator / $exercise->Norma, 2));
-                                }
-                                else {
-                                    echo((-1) * round( $numerator / $exercise->Norma, 2));
-                                }
-                            }
-                            else{
-                                echo(round(( $exercise->Value - $exercise->Norma) / $exercise->Norma, 2));
-                            }
-                        @endphp</div>
-                </div>
-            </div>
-            @php
-                if ($exercise->Name == "Ловля падающей линейки.")
-                {
-                    //dd($exercise);
-                    $OYFK += (-1) * round(( $exercise->Value - $exercise->Norma) / $exercise->Norma, 2);
-                }
-                else
-                    $OYFK += round(( $exercise->Value - $exercise->Norma) / $exercise->Norma, 2);
-            @endphp
-        @endforeach
-
-        @php $OYFK = $OYFK / 7; @endphp
     </div>
 
-<div class="result_total">
-    <h3>Общий уровень развития физических кондиций</h3>
-    <div class="total_counting">ОУФК = (О + ПТ + Н + У + ЛЛ + УР + ТТ) : 7 = {{round($OYFK, 2)}}</div>
+    <div class="row">
+        <div class="result_total">
+            <h3>Общий уровень развития физических кондиций</h3>
+            <div class="total_counting">ОУФК = (О + ПТ + Н + У + ЛЛ + УР + ТТ) : 7 = {{round($OYFK, 2)}}</div>
+        </div>
+    </div>
 </div>
+
--- laravel/resources/views/task6.blade.php
+++ laravel/resources/views/task6.blade.php
@@ -1,43 +1,37 @@
 <div class="result_title"><h2>Результаты тестирования</h2></div>
-<div class="container">
-    <br><h4><center>Рабочая таблица для определения суточного расхода энергии</center></h4>
-</div>
+<div class="result_title"><h2>Рабочая таблица для определения суточного расхода энергии</h2></div>
 @php
 $sum = 0;
 @endphp
-<div class='container statistic_table'>
-    <div class='row'>
-        <div class='col col-md-4'>
-            <div class='title_field_head'>Вид деятельности</div>
-        </div>
-        <div class='col col-md-2'>
-            <div class='title_field'>Время</div>
-        </div>
-        <div class='col col-md-3'>
-            <div class='title_field'>Продолжительность, мин</div>
-        </div>
-        <div class='col col-md-3'>
-            <div class='title_field'>Расход энергии(ккал на 1 кг веса тела)</div>
+
+<div class="container">
+    <div class="row">
+        <div class="table-responsive">
+            <table class="table table-bordered border-dark align-middle text-center">
+                <thead class="align-middle">
+                <tr>
+                    <th scope="col" class="test6">Вид деятельности</th>
+                    <th scope="col" class="test6">Время</th>
+                    <th scope="col" class="test6">Продолжительность, мин</th>
+                    <th scope="col" class="test6">Расход энергии(ккал на 1 кг веса тела)</th>
+                </tr>
+                </thead>
+                <tbody id="myTable" style="line-height: 2; white-space: normal;">
+                @foreach($result->Exercises as $key => $exercise)
+                    <tr>
+                        <th scope="row" class="test6">{{$exercise->Name}}</th>
+                        <td class="test6">{{$exercise->Description}}</td>
+                        <td class="test6">{{$exercise->Value}}</td>
+                        <td class="test6">{{$exercise->Norma}}</td>
+                    </tr>
+                    @php $sum += $exercise->Norma; @endphp
+                @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
-    @foreach($result->Exercises as $key => $exercise)
-        <div class='row'>
-            <div class='col col-md-4'>
-                <div class='data_field_head'>{{$exercise->Name}}</div>
-            </div>
-            <div class='col col-md-2'>
-                <div class='data_field'>{{$exercise->Description}}</div>
-            </div>
-            <div class='col col-md-3'>
-                <div class='data_field'>{{$exercise->Value}}</div>
-            </div>
-            <div class='col col-md-3'>
-                <div class='data_field'>{{$exercise->Norma}}</div>
-            </div>
-        </div>
-        @php $sum += $exercise->Norma; @endphp
-    @endforeach
-</div>
-<div class="container">
-    <br><h4><center>Итог:{{$sum}}</center></h4>
+
+    <div class="result_total">
+        <h3>Итог:{{$sum}}</h3>
+    </div>
 </div>
